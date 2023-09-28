8d7a7e14f7b8d2ba16037380b86b03aaae68e179
temasiot <artem.stukalo@gmail.com>
2023-09-28 09:10:47 +0300

    uncoment z2 in pr2

--- pr2.py
+++ pr2.py
@@ -6,7 +6,7 @@ from math import sqrt
 a = float(input())  # ввод зміної з клавіатури
 
 z1 = cos(3/8*pi - a/4)**2 - cos(11/8*pi + a/4)**2  # розрахунок z1
-# z2 = sqrt(2)/2 * sin(a/2)  # розрахунок z2
+z2 = sqrt(2)/2 * sin(a/2)  # розрахунок z2
 
 print('z1=', z1)  # вивід z1
-# print('z2=', z2)  # вивід z2
+print('z2=', z2)  # вивід z2
